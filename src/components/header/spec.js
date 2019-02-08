ef8269cda2ebd4f846f8bc88d1045d8725ab4b3f
mattperkins <github@mattperkins.com>
2019-02-08 16:31:11 +0000

    configure and instantiate new enzyme adapter

--- src/components/header/spec.js
+++ src/components/header/spec.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Enzyme, { shallow } from 'enzyme'
 import Header from './index'
 
+Enzyme.configure({ adapter: new Adapter() })
 describe('Header Component', () => {
 
-
   it('It should render without errors', () => {
-    expect(1).toBe(1)
+
+    // shallow render of component
+
+    const component = shallow(<Header />)
+    const wrapper = component.find('.HeaderComponent')
+    expect(wrapper.length).toBe(1)
   })
 
 
