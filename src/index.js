8ccc4c125d02c96c2bcb6b5b0d5f6ce6bc5a3a94
mattperkins <github@mattperkins.com>
2019-02-08 16:10:33 +0000

    import scss file

--- src/index.js
+++ src/index.js
@@ -1,33 +1,25 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
-// Modest Grid linked via public/index.html
-import './css/reset.css'
-import './css/style.css'
+import './index.scss'
+import Header from './components/header';
 
 // main ("ROOT") component 
 class Root extends React.Component {
 
-render() {
+  render() {
 
-return (
+    return (
 
-
-<div className="wrapper">
-<div className="row">
-
-<div className="col-12">
-  <h1>Hello, World!</h1>
-</div>
-
-</div>
-</div>
+      <>
+        <Header />
+      </>
 
 
-)// end return
-}// end render
+    )// end return
+  }// end render
 }// end component
 
-ReactDOM.render(<Root />, 
+ReactDOM.render(<Root />,
   document.getElementById('root'))
-  registerServiceWorker()
+registerServiceWorker()
