5c0be1a9cbe002fd4a1bc9817a5c40862a1637b4
mattperkins <github@mattperkins.com>
2019-02-10 12:32:34 +0000

    refactor: using beforeEach()

--- src/components/header/spec.js
+++ src/components/header/spec.js
@@ -12,20 +12,22 @@ const setUp = (props = {}) => {
 
 describe('Header Component', () => {
 
+  let component
+  beforeEach(() => {
+    component = setUp()
+  })
+
   it('should render without errors', () => {
 
     // shallow render of component
-
-    const component = setUp()
     const wrapper = component.find('.headerComponent')
     expect(wrapper.length).toBe(1)
 
-    console.log(wrapper.debug())
+    // console.log(wrapper.debug())
   })
 
   it('should render logo', () => {
 
-    const component = shallow(<Header />)
     const wrapper = component.find('.logoIMG')
     expect(wrapper.length).toBe(1)
   })
