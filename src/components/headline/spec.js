a226c8863195eef36175c59d6d54ada506aeef0e
mattperkins <github@mattperkins.com>
2019-02-11 16:54:23 +0000

    configure and instantiate test

--- src/components/headline/spec.js
+++ src/components/headline/spec.js
@@ -1,19 +1,32 @@
+import React from 'react'
 import Adapter from 'enzyme-adapter-react-16'
 import Enzyme, { shallow } from 'enzyme'
 import Headline from './index'
+import { findByTestAtrr } from '../../../Utils'
 
 Enzyme.configure({ adapter: new Adapter() })
 
 const setUp = (props = {}) => {
-  const component = shallow(<Headerline {...props} />)
+  const component = shallow(<Headline {...props} />)
   return component
 }
 
-it('should render without errors', () => {
+describe('Headline Component', () => {
 
-  // shallow render of component
-  const wrapper = findByTestAtrr(component, 'headline')
-  expect(wrapper.length).toBe(1)
+  let component
+  beforeEach(() => {
+    component = setUp()
+  })
 
-  // console.log(wrapper.debug())
+  it('should render without errors', () => {
+
+    // shallow render of component
+    const wrapper = findByTestAtrr(component, 'headline')
+    expect(wrapper.length).toBe(1)
+
+    // console.log(wrapper.debug())
+  })
+
+
+
 })
